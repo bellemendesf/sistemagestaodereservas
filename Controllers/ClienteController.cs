7920aeafd763abed732969e8be446a90fc1150a5
Isabelle Fatec <isabelle47mendes@gmail.com>
2026-09-22 17:59:30 -0300

    correcao bug controller

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using sistemagestaodereserva.Models;
 
 public class ClienteController : Controller
 {
--- Controllers/FuncionarioController.cs
+++ Controllers/FuncionarioController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using sistemagestaodereserva.Models;
 
 public class FuncionarioController : Controller
 {
