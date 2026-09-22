6101e7491a604a754baeb44717450bf8d2c1847e
Pedro Henrique <pedrohmfsilva@gmail.com>
2026-09-22 17:57:10 -0300

    Create: classes controllers

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
+using sistemagestaodereserva.Models;
+
+public class ClienteController : Controller
+{
+   
+}
---
+++ Controllers/MesaController.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
+using sistemagestaodereserva.Models;
+
+public class MesaController : Controller
+{
+   
+}
