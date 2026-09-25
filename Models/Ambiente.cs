02ebf48b241bb2f56e3288f8687cde233b05ea61
bellemendesf <isabelle47mendes@gmail.com>
2026-09-24 21:59:55 -0300

    fix: remove atributo status de ambiente

--- Models/Ambiente.cs
+++ Models/Ambiente.cs
@@ -4,6 +4,5 @@ public  class Ambiente
 {
     public int Id {get;set;}
     public required string Nome {get;set;}
-    public required string Status {get;set;}
     public required int Capacidade {get;set;}
 }
